4c5946f5d1fcd5765e433dccf1a7d6e8221cca44
Nick Troshnev <nik.troshnev@gmail.com>
2022-07-25 14:02:53 +0200

    Add bot main logic; Add slash commands interface

--- sentinel_discord_bot_v2/__init__.py
+++ sentinel_discord_bot_v2/__init__.py
@@ -1,2 +1,37 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
+__all__ = []
 
+from loguru import logger
+import config
+
+import discord
+
+from command_groups import punishments, amnesties
+
+bot = discord.Bot(debug_guilds=[827116545724383242])
+bot.add_application_command(punishments.group)
+bot.add_application_command(amnesties.group)
+
+
+@bot.event
+async def on_ready():
+    logger.success("bot is ready")
+
+
+@bot.event
+async def on_connected():
+    logger.success("connected to discord")
+
+
+@bot.event
+async def on_error(event: str):
+    match event:
+        case "on_connect":
+            logger.error(f"{event}: Failed to connect to discord servers")
+            await bot.close()
+        case _:
+            logger.error(f"{event}: Error occurred")
+
+
+if __name__ == "__main__":
+    bot.run(token=config.DISCORD_TOKEN)
